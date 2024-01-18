b7554c8367d2561069e4e3150cc18a2db76aec95
karnarajbanshi <67020608+karnarajbanshi@users.noreply.github.com>
2024-01-18 22:51:34 +0545

    Update build.sh
    added some additional scripts as per  my requirement

--- docker/docker-examples/build.sh
+++ docker/docker-examples/build.sh
@@ -8,8 +8,12 @@ pwd
 #cd docker/docker-examples
 # Define variables
 APP_NAME="my-app"
-BUILD_NUMBER="$1"
+BUILD_NUMBER="$1"  #environment varibale that denotes built number.
+
+# cleans up all unused images, containers, networks, and volumes, effectively clearing the cache before build
+#use with caution and only if neccessary
+docker system prune -a --volumes
 
 # Build the Docker image
-docker system prune -a --volumes
+# --no-cache flag explicitly instructs Docker to ignore any cached layers and build all layers from the beginning.
 docker build --no-cache -t "$APP_NAME:$BUILD_NUMBER" . -f Dockerfile
