b0d05340c25a66b16893bc02cd9256184dae0693
srtimsina <timsinasuryaraj@gmail.com>
2024-09-10 15:13:12 +0545

    Update nexus-setup.sh
    Update nexus-setup.sh

--- jenkins/java-tomcat-sample/userdata/nexus-setup.sh
+++ jenkins/java-tomcat-sample/userdata/nexus-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install java-1.8.0-openjdk.x86_64 wget -y
+sudo apt install openjdk-17-jdk wget -y
 mkdir -p /opt/nexus/   
 mkdir -p /tmp/nexus/                           
 cd /tmp/nexus/
