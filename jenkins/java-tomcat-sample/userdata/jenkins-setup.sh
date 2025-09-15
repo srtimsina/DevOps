0311eafb9a406b3f65405fe35de26a5cc8c3eb26
srtimsina <timsinasuryaraj@gmail.com>
2025-09-15 14:38:03 +0545

    Update jenkins-setup.sh

--- jenkins/java-tomcat-sample/userdata/jenkins-setup.sh
+++ jenkins/java-tomcat-sample/userdata/jenkins-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install fontconfig openjdk-21-jre
+sudo apt install fontconfig openjdk-21-jre -y
 sudo apt install maven -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
